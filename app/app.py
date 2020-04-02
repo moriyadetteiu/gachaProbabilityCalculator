0aac4f59fd4023cc36f16900ecc775efbad41afd
detteiu <detteiu.stg@gmail.com>
2020-04-03 02:15:41 +0900

    feat: 仮の数値での計算を実装

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
-print("hello world")
+from fractions import Fraction
+import calculator.expected_value as expected_value
+
+limitedWeaponPerAll = 0.043 / 100
+TotalNumberOfLimitedWeapon = 12
+
+angelStonePerAll = 0.013 / 100
+TotalNumberOfAngelStone = 6
+
+hitPerTrial =Fraction(str(limitedWeaponPerAll * TotalNumberOfLimitedWeapon))
+results = (
+    float(expected_value.calc_expected_value(hitPerTrial, 300, n))
+    for n
+    in range(0, 300)
+)
+
+print(list(results))
